8e4c2ea596b755e33294081c7e1fac74e2388de7
Anmol Saxena <anmolsaxena456@gmail.com>
2021-10-21 00:10:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ class Audio {
 		
 		this.fileReader.onload = function() {
 			_this.audioContext.decodeAudioData(_this.fileReader.result, function(buffer) {
+				console.log(_this);
 				if (_this.source) {
 					_this.source.stop();
 				}
@@ -382,15 +383,3 @@ class Webgl {
 		this.renderer.render(this.scene, this.camera);
 	}
 }
-
-RGBA(`
-    vec2 uv = gl_FragCoord.xy/resolution - 0.5;
-    uv.x *= resolution.x/resolution.y; 
-    uv *= 10.;
-    float a = uv.x + sin(cos(time/5.)*19.*uv.y);
-    float b = uv.y - sin(cos(time/5.)*19.*uv.x);
-    float f = a*sin(time) - b*cos(time);
-    float v = smoothstep(.3, .7, f);
-    gl_FragColor = vec4(v, v, v, 1.);
-`);
-
